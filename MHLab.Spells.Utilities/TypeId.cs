e016141c1c271cc82b477cd7fdb0fa055d47ee91
Emanuele Manzione <m4nu.91@gmail.com>
2022-04-09 16:10:36 +0200

    update: added the Utilities project

---
+++ MHLab.Spells.Utilities/TypeId.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace MHLab.Spells.Utilities
+{
+    public readonly struct TypeId : IEquatable<TypeId>
+    {
+        private readonly int _id;
+
+        internal TypeId(int id)
+        {
+            _id = id;
+        }
+        
+        public bool Equals(TypeId other)
+        {
+            return _id == other._id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is TypeId other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return _id;
+        }
+
+        public static bool operator ==(TypeId left, TypeId right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(TypeId left, TypeId right)
+        {
+            return !left.Equals(right);
+        }
+    }
+    
+    public static class TypeMapper<TType>
+    {
+        public static readonly TypeId Id = TypeIdGenerator.Get();
+    }
+
+    internal static class TypeIdGenerator
+    {
+        private static int _counter;
+
+        public static TypeId Get()
+        {
+            var counter = _counter;
+            _counter++;
+            return new TypeId(counter);
+        }
+    }
+}
